3e892999242586fed0c0dee3281c19909a5eb07e
Tyler Lowrey <contact@tylerlowrey.com>
2019-12-14 17:54:00 -0500

    Initial commit

---
+++ src/main/java/org/team283/frcscoutingwebapp/FrcScoutingWebApplication.java
@@ -0,0 +1,15 @@
+package org.team283.frcscoutingwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrcScoutingWebApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FrcScoutingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/team283/frcscoutingwebapp/FrcScoutingWebApplicationTests.java
@@ -0,0 +1,14 @@
+package org.team283.frcscoutingwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrcScoutingWebApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
