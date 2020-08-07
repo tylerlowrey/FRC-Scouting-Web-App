61f5f7934e35e71f97efd71d4a822f20345cf1be
Tyler Lowrey <contact@tylerlowrey.com>
2020-08-07 17:32:51 -0400

    Added Home route and Home component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
