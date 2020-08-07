11cd9cf8fe38db46a44bbaa508885a871cc7cedd
Tyler Lowrey <contact@tylerlowrey.com>
2020-08-07 19:53:07 -0400

    Finished basic home page ui without functionality

--- webapp/src/components/LoginForm/index.js
+++ webapp/src/components/LoginForm/index.js
@@ -4,7 +4,16 @@ import localStyles from './loginForm.module.css';
 const LoginForm = () => {
   return(
     <div className={localStyles.container}>
-        <p>Login form</p>
+        <div className={localStyles.loginForm}>
+            <p className={localStyles.loginFormTitle}>Login</p>
+
+            <label htmlFor="username">Email</label>
+            <input name="username" type="text"/>
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password"/>
+
+            <button className={localStyles.loginButton}>Login</button>
+        </div>
     </div>
   )
 }
