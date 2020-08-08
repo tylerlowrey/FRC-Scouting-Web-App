30563b18f75efe63db0e2b1f14dd6e2e67ef84be
Tyler Lowrey <contact@tylerlowrey.com>
2020-08-08 17:37:06 -0400

    Created usersService skeleton

---
+++ webapp/src/services/users.service.js
@@ -0,0 +1,18 @@
+export const usersService = {
+    login,
+    logout,
+    register
+};
+
+function login(username, password) {
+
+}
+
+function logout() {
+
+}
+
+function register(username, password) {
+    
+}
+
