15d5a46c37fd970c1a1b2c0bba9d4866b02c59f0
Tyler Lowrey <contact@tylerlowrey.com>
2020-08-14 16:18:29 -0400

    Added Spring Initialzr project to scouting-data-service

---
+++ scouting-data-service/src/test/java/org/team283/scoutingdataservice/ScoutingDataServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.team283.scoutingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutingDataServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
